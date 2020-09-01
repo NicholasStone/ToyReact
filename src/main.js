f807ed9390e92b6f71c91351e71b3ea0bba51c0b
NicholasStone <Nicholas-Stone@outlook.com>
2020-09-01 10:11:31 +0800

    追加注释

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import {createElement, render, Component} from './toy-react'
 
 class Comp extends Component{
   render() {
+    // 这样写只能展示下面的 Comp, 不能将 10 行开始的 JSX 内容完全展示
     return <div>Comp</div>
   }
 }
